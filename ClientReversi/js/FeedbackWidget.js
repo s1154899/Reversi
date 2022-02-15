44d551e03a900c855ca102d94e0e9ff742d5a650
s1154899 <74304795+s1154899@users.noreply.github.com>
2022-02-15 10:07:59 +0100

    start module pattern feedback widget

--- ClientReversi/js/FeedbackWidget.js
+++ ClientReversi/js/FeedbackWidget.js
@@ -84,3 +84,78 @@ function toggleFeedback(elementId) {
         x.style.display = "none";
     }
 }
+
+var FeedbackWidget = (function(){
+    var _elementId;
+    function init(){
+
+        _elementId = elementId;
+        var Element = document.getElementById(this.elementId);
+        var Buttons = Element.getElementsByTagName("button");
+
+        var FakeHide = this.hide;
+        for(var i = 0; i < Buttons.length; i++) {
+            Buttons[i].addEventListener( "click",function(){FakeHide(elementId);});
+        }
+
+    }
+
+    function show(type,message){
+        this.log(type,message);
+        var container = document.getElementById(this._elementId);
+        switch (type) {
+            case "succes":
+                container.className = "alert alert-success";
+                break;
+            default:
+                container.className = "alert alert-danger";
+                break;
+        }
+
+        container.getElementsByTagName("p")[0].innerText = message;
+        if (container.style.display != "block"){
+            toggleFeedback(this._elementId);
+        }
+    }
+    function hide(element){
+        if (document.getElementById(element).style.display != "none"){
+            toggleFeedback(element);
+        }
+    }
+    function log(type,message){
+        //localStorage.setItem()
+
+        var obj = {message , type};
+        obj.message = message;
+        obj.type = type;
+
+
+        var obj2 = [];
+        var feedbackJson =JSON.parse( localStorage.getItem("feedback_widget"));
+        obj2.push(obj);
+        if(feedbackJson != null) {
+            for (var i = 0; (i < feedbackJson.length) && (i < 10 ); i++) {
+
+                obj2.push(feedbackJson[i]);
+            }
+        }
+
+        localStorage.setItem("feedback_widget",JSON.stringify(obj2));
+
+    }
+    function removeLog(){
+
+        localStorage.removeItem('feedback_widget');
+    }
+
+    function history(){
+        return JSON.parse( localStorage.getItem("feedback_widget"));
+
+    }
+
+    return {}
+})(elementId);
+
+
+//TODO make return work
+//TODO remove the class
