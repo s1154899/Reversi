997f068da83caa0ef359d148ca0235569b8c1896
s1154899 <s1154899@student.windesheim.nl>
2022-02-15 19:10:35 +0100

    level 2-7

--- ClientReversi/js/FeedbackWidget.js
+++ ClientReversi/js/FeedbackWidget.js
@@ -1,108 +1,29 @@
 
-class FeedbackWidget{
-    constructor(elementId) {
 
-
-        this._elementId = elementId;
-        var Element = document.getElementById(this.elementId);
-        var Buttons = Element.getElementsByTagName("button");
-
-        var FakeHide = this.hide;
-        for(var i = 0; i < Buttons.length; i++) {
-            Buttons[i].addEventListener( "click",function(){FakeHide(elementId);});
-        }
-
-
-        //$("#feedback-success-accept").on("click", function(){
-        //    alert("The button was clicked.");
-        //});
-    }
-
-    get elementId() { //getter, set keyword voor setter methode
-        return this._elementId;
-    }
-
-    show(type,message){
-        this.log(type,message);
-        var container = document.getElementById(this._elementId);
-        switch (type) {
-            case "succes":
-                container.className = "alert alert-success";
-                break;
-            default:
-                container.className = "alert alert-danger";
-                break;
-        }
-
-        container.getElementsByTagName("p")[0].innerText = message;
-        if (container.style.display != "block"){
-            toggleFeedback(this._elementId);
-        }
-    }
-    hide(element){
-        if (document.getElementById(element).style.display != "none"){
-            toggleFeedback(element);
-        }
-    }
-    log(type,message){
-        //localStorage.setItem()
-
-        var obj = {message , type};
-        obj.message = message;
-        obj.type = type;
-
-
-        var obj2 = [];
-        var feedbackJson =JSON.parse( localStorage.getItem("feedback_widget"));
-        obj2.push(obj);
-        if(feedbackJson != null) {
-            for (var i = 0; (i < feedbackJson.length) && (i < 10 ); i++) {
-
-                obj2.push(feedbackJson[i]);
-            }
-        }
-
-        localStorage.setItem("feedback_widget",JSON.stringify(obj2));
-
-    }
-    removeLog(){
-
-        localStorage.removeItem('feedback_widget');
-    }
-
-    history(){
-        return JSON.parse( localStorage.getItem("feedback_widget"));
-
-    }
-
-}
-function toggleFeedback(elementId) {
-    var x = document.getElementById(elementId);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
 
 var FeedbackWidget = (function(){
     var _elementId;
+
+    function SetElementId(elementId){
+        _elementId = elementId;
+        init();
+    }
+
     function init(){
 
-        _elementId = elementId;
-        var Element = document.getElementById(this.elementId);
+        var Element = document.getElementById(_elementId);
         var Buttons = Element.getElementsByTagName("button");
 
-        var FakeHide = this.hide;
+
         for(var i = 0; i < Buttons.length; i++) {
-            Buttons[i].addEventListener( "click",function(){FakeHide(elementId);});
+            Buttons[i].addEventListener( "click",function(){hide(_elementId);});
         }
 
     }
 
     function show(type,message){
-        this.log(type,message);
-        var container = document.getElementById(this._elementId);
+        LocalLogging.log("feedback_widget",type,message);
+        var container = document.getElementById(_elementId);
         switch (type) {
             case "succes":
                 container.className = "alert alert-success";
@@ -114,7 +35,7 @@ var FeedbackWidget = (function(){
 
         container.getElementsByTagName("p")[0].innerText = message;
         if (container.style.display != "block"){
-            toggleFeedback(this._elementId);
+            toggleFeedback(_elementId);
         }
     }
     function hide(element){
@@ -122,39 +43,29 @@ var FeedbackWidget = (function(){
             toggleFeedback(element);
         }
     }
-    function log(type,message){
-        //localStorage.setItem()
 
-        var obj = {message , type};
-        obj.message = message;
-        obj.type = type;
-
-
-        var obj2 = [];
-        var feedbackJson =JSON.parse( localStorage.getItem("feedback_widget"));
-        obj2.push(obj);
-        if(feedbackJson != null) {
-            for (var i = 0; (i < feedbackJson.length) && (i < 10 ); i++) {
-
-                obj2.push(feedbackJson[i]);
-            }
+    function toggleFeedback(elementId) {
+        var x = document.getElementById(elementId);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
         }
-
-        localStorage.setItem("feedback_widget",JSON.stringify(obj2));
-
-    }
-    function removeLog(){
-
-        localStorage.removeItem('feedback_widget');
     }
 
-    function history(){
-        return JSON.parse( localStorage.getItem("feedback_widget"));
+
+
+    return {
+        show : show,
+        hide : hide,
+        SetElementId : SetElementId,
+
+        log : function(){LocalLogging.log("feedback_widget");},
+        removeLog : function(){LocalLogging.removeLog("feedback_widget");},
+        history : function(){LocalLogging.history("feedback_widget");}
 
     }
-
-    return {}
-})(elementId);
+})();
 
 
 //TODO make return work
---
+++ ClientReversi/js/Game.Model.js
@@ -0,0 +1,15 @@
+Game.Model = (function(){
+
+    //Configuratie en state waarden
+    let configMap = {
+        apiUrl: url
+    }
+
+    function init(){}
+
+    return{
+
+        init : init
+    }
+
+})();
