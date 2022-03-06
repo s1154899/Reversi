bb88cc59c12b5e91be03836808f53b2b1cce63ac
s1154899 <s1154899@student.windesheim.nl>
2022-03-06 16:22:27 +0100

    Update SpelRepository.cs

--- ReversiRestApi/ReversiRestApi/Model/SpelRepository.cs
+++ ReversiRestApi/ReversiRestApi/Model/SpelRepository.cs
@@ -12,6 +12,7 @@ namespace ReversiRestApi.Model
             Spel spel1 = new Spel();
             Spel spel2 = new Spel();
             Spel spel3 = new Spel();
+            Spel spel4 = new Spel();
 
             spel1.Speler1Token = "abcdef";
             spel1.Omschrijving = "Potje snel reveri, dus niet lang nadenken";
@@ -20,9 +21,11 @@ namespace ReversiRestApi.Model
             spel2.Omschrijving = "Ik zoek een gevorderde tegenspeler!";
             spel3.Speler1Token = "stuvwx";
             spel3.Omschrijving = "Na dit spel wil ik er nog een paar spelen tegen zelfde tegenstander";
+            spel4.Speler1Token = "stuvwx";
+            spel4.Omschrijving = "Na dit spel wil ik er nog een paar spelen tegen zelfde tegenstander";
 
 
-            Spellen = new List<Spel> { spel1, spel2, spel3 };
+            Spellen = new List<Spel> { spel1, spel2, spel3,spel4 };
         }
 
         public void AddSpel(Spel spel)
