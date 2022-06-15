550503f065ae6c32a09fc50009fea8e6739dcace
s1154899 <s1154899@student.windesheim.nl>
2022-06-15 19:09:45 +0200

    captcha

--- ReversiRestApi/ReversiMvcApp/Program.cs
+++ ReversiRestApi/ReversiMvcApp/Program.cs
@@ -3,7 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using ReversiMvcApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+/*
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
+*/
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
